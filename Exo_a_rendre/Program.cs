78436c3cd6f70643494a0396467eea9f51381ff8
fatima12d <fatimamouhamed586@gmail.com>
2024-01-05 16:24:31 +0000

    premier commit

---
+++ Exo_a_rendre/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo_a_rendre
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Donner le montant des dommages :");
+            double montant = double.Parse(Console.ReadLine());
+            Console.WriteLine("le montant est :" + montant);
+            double franc = (montant * 10) / 100;
+            if( franc <=4000 ) {
+                montant -= franc;
+                Console.WriteLine("le montant a rembourser est :" + montant);
+                Console.WriteLine("la franchise est de :" + franc);
+            }
+            else
+            {
+                montant -= 4000;
+                Console.WriteLine("le montant a rembourser est :" + montant);
+                Console.WriteLine("la franchise est de :" + 4000);
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
